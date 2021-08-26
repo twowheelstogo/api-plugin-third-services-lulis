8fe05b26ccb2a872c35f39cf1186dedaa02c2f0b
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2021-08-26 05:43:20 -0600

    fix distance

--- src/services/distance-matrix.service.js
+++ src/services/distance-matrix.service.js
@@ -3,21 +3,18 @@ import fetch from "node-fetch";
 export default async (geopoint) => {
   const lulisGeopoint = process.env.LULIS_GEOPOINT;
   const googleKey = process.env.GOOGLE_KEY;
-  console.log(lulisGeopoint);
   let url = "https://maps.googleapis.com/maps/api/distancematrix/json";
   url += `?origins=${encodeURIComponent(`${geopoint.latitude},${geopoint.longitude}`)}`;
   url += `&destinations=${encodeURIComponent(`${lulisGeopoint}`)}`;
   url += `&region=${encodeURI("gt")}`;
   url += `&language=${encodeURI("es")}`;
   url += `&key=${encodeURI(googleKey)}`;
-  console.log(url);
   const res = await fetch(
     url,
     {
       method: "GET"
     }
   );
-  console.log("res.ok",res.ok);
   if (!res.ok) {
     return {
       text: "0 km",
@@ -25,22 +22,19 @@ export default async (geopoint) => {
     };
   }
   const data = await res.json();
-  console.log("data.status",data.status);
   if (data.status !== "OK") {
     return {
       text: "0 km",
       value: 0
     };
   }
-  console.log("data.rows[0]",data.rows[0]);
   if (data.rows[0]) {
-    console.log("data.rows[0].elements",data.rows[0].elements);
     if (data.rows[0].elements) {
-        console.log("data.rows[0].elements.distance",data.rows[0].elements.distance);
-      if (data.rows[0].elements.distance) {
-        data.rows[0].elements.distance.value /= 1000;
-        console.log("data.rows[0].elements.distance",data.rows[0].elements.distance);
-        return data.rows[0].elements.distance;
+      if (data.rows[0].elements[0]) {
+        if (data.rows[0].elements[0].distance) {
+          data.rows[0].elements[0].distance.value /= 1000;
+          return data.rows[0].elements[0].distance;
+        }
       }
     }
   }
